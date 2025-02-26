f9946574774c19544df45574b7b8debe87a058a0
devportela <portelagustavo947@gmail.com>
2025-02-26 10:57:57 -0300

    inital commit

---
+++ src/aulasjava/AulasJava.java
@@ -0,0 +1,11 @@
+nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aulasjava;
+
+public class AulasJava {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
