d69e74b0f3fd6b44701c4a6827f201c49977a6a2
GUSTAVO PORTELA DE LIMA <GUSTAVO_4657@uirapuru.br>
2025-04-09 11:44:47 -0300

    aula ds 09 04

--- src/java aulas/mesdeAbril/escola/aluno.java
+++ src/java aulas/mesdeAbril/escola/aluno.java
@@ -1,4 +1,32 @@
 package mesdeAbril.escola;
 
 public class aluno {
+
+
+    public String getNome() {
+        return nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public void enviarDados() {
+        System.out.println("aluno,nome é " + getNome());
+        System.out.println("aluno,sua idade é de:" + getidade());
+    }
+
+    public int getidade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    private String nome;
+    private int idade;
+
 }
--- src/java aulas/mesdeAbril/escola/escola.java
+++ src/java aulas/mesdeAbril/escola/escola.java
@@ -1,4 +1,16 @@
 package mesdeAbril.escola;
 
-public class escola {
+public class escola  {
+    public static void main(String[] args) {
+      aluno cad = new aluno();
+
+      cad.setNome("Gustavo");
+      cad.setIdade(29);
+
+        System.out.println("Seu nome é:"+cad.getNome() + "Sua idade: "+cad.getidade());
+
+
+
+
+    }
 }
